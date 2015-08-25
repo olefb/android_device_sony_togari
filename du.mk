9fe0f3e7514780694c4e8625927a76fdbf5184cd
Ole Birketvedt <fossbab@gmail.com>
2015-08-25 13:37:14 +0200

    Bringup for Dirty Unicorns

--- du.mk
+++ du.mk
@@ -1,8 +1,8 @@
 # Inherit CM common Phone stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/du/config/nfc_enhanced.mk)
 
 $(call inherit-product, device/sony/togari/full_togari.mk)
 
@@ -10,5 +10,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6833
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/C6833/C6833:5.0.2/14.5.A.0.270/3305956307:user/release-keys
 PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="C6833-user 5.0.2 14.5.A.0.270 3305956307 release-keys"
 
-PRODUCT_NAME := cm_togari
+PRODUCT_NAME := du_togari
 PRODUCT_DEVICE := togari
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,2 @@
-add_lunch_combo cm_togari-eng
+add_lunch_combo du_togari-eng
+add_lunch_combo du_togari-userdebug
